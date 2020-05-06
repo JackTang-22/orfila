58b5f8c907b5924da937248ac55ecf61ab444e40
tangye <ye.tang@mintegral.com>
2020-05-06 22:53:19 +0800

    StringType

---
+++ base/src/test/java/com/orfila/string/StringTest.java
@@ -0,0 +1,21 @@
+package com.orfila.string;
+
+import org.junit.Test;
+
+/**
+ * @Description string test
+ * @Author tangye
+ * @Time 2020-05-06 22:28
+ */
+public class StringTest {
+
+    @Test
+    public void stringTest() {
+        StringType.testString();
+    }
+
+    @Test
+    public void stringTest1() {
+        StringType.testString1();
+    }
+}
